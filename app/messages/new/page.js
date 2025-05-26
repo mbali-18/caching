d096bb9035dfa2ea7b2d1c149709162cb390c4f1
mbali-18 <faithfulservent18@gmail.com>
2025-05-27 00:51:25 +0200

    invalidating custom data source data

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -10,7 +10,8 @@ export default function NewMessagePage() {
 
     const message = formData.get("message");
     addMessage(message);
-    // revalidateTag("msg");
+    // revalidatePath("/messages");
+    revalidateTag("msg");
     redirect("/messages");
   }
 
