e8140ef1fa9e9bb5160e27d0a046198e823b6f14
mbali-18 <faithfulservent18@gmail.com>
2025-05-26 23:48:06 +0200

    controlling data caching

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,10 +1,12 @@
+import { unstable_noStore } from "next/cache";
 import Messages from "@/components/messages";
 
+// export const revalidate = 5;
+//export const dynamic ='force dynamic';
+
 export default async function MessagesPage() {
+  unstable_noStore();
   const response = await fetch("http://localhost:8080/messages", {
-    next: {
-      revalidate: 5,
-    },
     // headers: {
     //   "X-ID": "page",
     // },
